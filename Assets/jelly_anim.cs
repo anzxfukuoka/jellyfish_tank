12d0c8af7bef0928f716f4e3a2e64c634f0b4bcf
Anzx <0.love.2d.atsi.2@gmail.com>
2020-08-10 03:33:42 +0300

    ♥

---
+++ Assets/jelly_anim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jelly_anim : MonoBehaviour
+{
+
+    public float time = 0;
+
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public Mesh skinnedMesh;
+
+    public float speed = 6f;
+    public float step = 6f;
+
+    void Awake()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
+    }
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        skinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.Sin(time * speed) * 50 + 50);
+        skinnedMeshRenderer.SetBlendShapeWeight(1, Mathf.Cos(time * speed) * 50 + 50);
+
+        transform.Translate(new Vector3(0, 0, Mathf.Cos(time * speed + Mathf.PI/2) * step + step / 2), Space.Self);
+    }
+}
---
+++ Assets/spawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject jellyPrefab;
+
+    public Vector3 rotAngle;
+
+    public Vector3 rotSpeed;
+
+    public float spawnDelay = 1f;
+
+    public Vector3 spawnRect;
+
+    public GameObject[] jellyList;
+
+    float upperEdge;
+    float lowerEdge;
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(0.6f, 0, 0.8f, 0.4f);
+        Gizmos.DrawCube(transform.position + spawnRect/2, spawnRect);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lowerEdge = transform.position.y;
+        upperEdge = transform.position.y + spawnRect.y;
+
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn()
+    {
+        for (int i = 0; i < jellyList.Length; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(transform.position.x , transform.position.x + spawnRect.x), Random.Range(transform.position.y, transform.position.y + spawnRect.y), Random.Range(transform.position.z, transform.position.z + spawnRect.z));
+
+            GameObject j = Instantiate(jellyPrefab, position, Quaternion.Euler(rotAngle));
+            jellyList[i] = j;
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotAngle += rotSpeed;
+
+        for (int i = 0; i < jellyList.Length; i++)
+        {
+            jellyList[i].transform.rotation = Quaternion.Euler(rotAngle);
+
+            if (jellyList[i].transform.position.y > upperEdge)
+                jellyList[i].transform.position = new Vector3(jellyList[i].transform.position.x, lowerEdge, jellyList[i].transform.position.z);
+
+            if (jellyList[i].transform.position.y < lowerEdge)
+                jellyList[i].transform.position = new Vector3(jellyList[i].transform.position.x, upperEdge, jellyList[i].transform.position.z);
+        }
+    }
+}
